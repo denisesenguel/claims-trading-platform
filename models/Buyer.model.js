4cb5cbfd3e95487af178527996773c26e0374091
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-02-14 14:24:52 +0100

    added buyerModel

---
+++ models/Buyer.model.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+const buyerSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "Email is required."]
+    },
+    affiliation: {
+      type: String,
+      trim: true
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Buyer = model("Buyer", buyerSchema);
+
+module.exports = Buyer;
