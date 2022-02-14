440faae0a8fee197e457b7f7fef5e673feb5562e
denisesenguel <denisesenguel@gmail.com>
2022-02-14 14:29:58 +0100

    add Seller model

---
+++ models/Seller.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const sellerSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      required: [true, "Email is required."],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "Email is required."]
+    },
+    affiliation: {
+      type: String,
+      trim: true
+    },
+    listedClaims: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Claim"
+      }
+    ]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Seller = model("Seller", sellerSchema);
+
+module.exports = Seller;
