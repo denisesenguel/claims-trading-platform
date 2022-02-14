869b09b592357288fdd7dc745b26d77720ba1047
denisesenguel <denisesenguel@gmail.com>
2022-02-14 14:55:49 +0100

    add Claims model

---
+++ models/Claim.model.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+
+const claimSchema = new Schema(
+  {
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true
+    },
+    debtor: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    debtorLocation: {
+      type: String,
+      required: true
+      // eventually add country enum but we'll try to solve via HTML 
+    },
+    faceValue: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    currency: {
+      type: String,
+      // eventually remove and check via HTML form
+      enum: ['EUR', 'USD', 'GBP'],
+      required: true
+    },
+    type: {
+      type: String,
+      enum: ['Corporate', 'Consumer', 'Retail Mortgage', 'Commercial Real Estate', 'Liability'],
+      required: true
+    },
+    minimumPrice: {
+      type: Number,
+      min: 0,
+      max: 100
+    },
+    performance: {
+      type: String,
+      enum: ['performing', 'defaulted', 'stressed']
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Claim = model("Claim", claimSchema);
+
+module.exports = Claim;
