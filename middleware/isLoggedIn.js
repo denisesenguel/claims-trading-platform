e7ae5395d14aea1704c15203bb5d457382e9403b
denisesenguel <denisesenguel@gmail.com>
2022-02-15 12:01:16 +0100

    add middleware for buyer/seller login check

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,19 @@
-module.exports = (req, res, next) => {
+isLoggedInAsBuyer = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
-  if (!req.session.user) {
+  if (!req.session.buyer) {
     return res.redirect("/auth/login");
   }
-  req.user = req.session.user;
+  req.buyer = req.session.buyer;
   next();
 };
+
+isLoggedInAsSeller = (req, res, next) => {
+  // checks if the user is logged in when trying to access a specific page
+  if (!req.session.seller) {
+    return res.redirect("/auth/login");
+  }
+  req.seller = req.session.seller;
+  next();
+};
+
+module.exports = {isLoggedInAsBuyer, isLoggedInAsSeller};
