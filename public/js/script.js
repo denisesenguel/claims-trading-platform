5839cafe19381b281807cc1cb0e3975d4b61cc84
denisesenguel <denisesenguel@gmail.com>
2022-02-21 10:57:57 +0100

    add headshake animation and prevent form submission on login form when fields are empty

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,21 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("claims-trading-platform JS imported successfully!");
-  },
-  false
-);
+const loginForm = document.getElementById("login-form");
+if (loginForm) {
+
+  const loginSubmit = loginForm.querySelector(".btn");
+  
+  loginSubmit.addEventListener("click", () => {
+
+    const radioElmArray = Array.from(loginForm.querySelectorAll("input.form-check-input"));
+    const radioElmSelected = radioElmArray.filter(inputField => inputField.checked);
+
+    const textFieldArray = Array.from(loginForm.querySelectorAll("input.form-control"));
+    const textFieldEmpty = textFieldArray.filter(inputField => inputField.value === "");
+
+    if (radioElmSelected.length < 1 || textFieldEmpty.length > 0) {
+      loginSubmit.classList.add('animate__animated', 'animate__headShake');
+      setTimeout(function() {
+        loginSubmit.classList.remove('animate__animated', 'animate__headShake');
+      }, 1000);
+    }
+  });
+}
